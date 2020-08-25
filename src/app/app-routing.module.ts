0dd3db61f2927abd46e76c811b4de8794232ddd8
jamesonojae <jamesonojae@gmail.com>
2020-08-25 19:47:05 +0100

    Screens painting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ContactsComponent} from './contacts/contacts.component';
+import {ProfilesComponent} from './profiles/profiles.component';
+import {SettingsComponent} from './settings/settings.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'profiles', component: ProfilesComponent},
+  {path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    ProfilesComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
